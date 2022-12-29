0c716a7a54391e85144ae68e744211aad04c5996
Jeremiah Agbama <agbamajeremiah@gmail.com>
2022-12-29 15:56:26 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Kindred Home Page'),
+      home: const MyHomePage(title: 'Kindred'),
     );
   }
 }
